40276811a0e146b5b4af3a4a1e9dc8d1da363412
parkyangji <dhsfid02@gmail.com>
2023-04-30 22:37:11 +0900

    Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import styled from "styled-components";
-import Circle from "./Circle";
+import { FormEvent, useState } from "react";
 
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: {value}, 
+    } = event;
+    setValue(value); //type=text => string
+  };
+  // any는 타입스크립트의 타입, 무엇이든 될 수 있다 but 지양
+  // 타입스크립트는 target > currentTarget
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(value);
+  };
+  // <HTMLInputElement> <HTMLFormElement> 어떤 녀석이 이 이벤트를 만들고 발생 시키겠는가
   return (
     <div>
-      <Circle bgColor={"teal"} borderColor="white" />
-      <Circle text="im here" bgColor={"tomato"} />
+      <form onSubmit={onSubmit}>
+        <input 
+        value={value} 
+        onChange={onChange} 
+        type="text" 
+        placeholder="username"/>
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
